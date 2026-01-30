50a8fdb43052bfe45d3a8099ce392bef7d0a0775
Officialcucler <cucler.in@gmail.com>
2026-01-30 19:07:07 +0530

    modified:   assets/css/main.css 	modified:   assets/js/include-components.js 	modified:   components/footer.html 	modified:   index.html 	modified:   pages/contact.html 	modified:   pages/privacy-policy.html 	modified:   pages/terms-of-service.html 	modified:   tools/image-resizer.html 	modified:   tools/pdf-compressor.html 	modified:   tools/signature-resizer.html

--- assets/js/include-components.js
+++ assets/js/include-components.js
@@ -14,10 +14,10 @@
   };
 
   await Promise.all([
-    load('#site-header', 'components/header.html'),
-    load('#site-footer', 'components/footer.html'),
-    load('#share', 'components/share.html'),
-    load('#move-to-top', 'components/move-to-top.html')
+    load('#site-header', '/components/header.html'),
+    load('#site-footer', '/components/footer.html'),
+    load('#share', '/components/share.html'),
+    load('#move-to-top', '/components/move-to-top.html')
   ]);
 
   window.__componentsLoaded = true;
