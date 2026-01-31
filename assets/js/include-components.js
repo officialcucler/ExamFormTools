2fc2c55ef2b5ab4ea5e4e1dd823d2026bc5b5052
Officialcucler <cucler.in@gmail.com>
2026-02-01 00:27:03 +0530

    modified:   assets/js/include-components.js

--- assets/js/include-components.js
+++ assets/js/include-components.js
@@ -1,22 +1,31 @@
 // Simple client-side include loader for header/footer components
 (function () {
-  const load = (selector, url) => {
+  const load = async (selector, url) => {
     const container = document.querySelector(selector);
     console.log('Loading', selector, 'container:', container);
     if (!container) return;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, false); // synchronous
-    xhr.send();
-    if (xhr.status === 200) {
-      container.innerHTML = xhr.responseText;
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const html = await response.text();
+        container.innerHTML = html;
+      }
+    } catch (error) {
+      console.error('Error loading component:', url, error);
     }
   };
 
-  load('#site-header', '../components/header.html');
-  load('#site-footer', '../components/footer.html');
-  load('#share', '../components/share.html');
-  load('#move-to-top', '../components/move-to-top.html');
+  const loadComponents = async () => {
+    await Promise.all([
+      load('#site-header', '../components/header.html'),
+      load('#site-footer', '../components/footer.html'),
+      load('#share', '../components/share.html'),
+      load('#move-to-top', '../components/move-to-top.html')
+    ]);
 
-  window.__componentsLoaded = true;
-  document.dispatchEvent(new CustomEvent('components:loaded'));
+    window.__componentsLoaded = true;
+    document.dispatchEvent(new CustomEvent('components:loaded'));
+  };
+
+  loadComponents();
 })();
